941ced10025ae26e187d35495e852c247985b559
jt174 <jt174@CESAR03>
2023-11-01 22:08:01 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+namespace Practica19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cal;
+            cal = double.Parse(C1.Text);
+            cal = double.Parse(C2.Text);
+            cal = double.Parse(C3.Text);
+            cal = double.Parse(C4.Text);
+
+
+            if (cal > 6) MessageBox.Show(" APROBADO ");
+            else MessageBox.Show(" REPROBADO ");
+        }
+    }
+}
